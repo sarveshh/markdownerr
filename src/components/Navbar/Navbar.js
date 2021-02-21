eff4f58c34e4bef9c41531a2f54a34a46d88fd4f
sarveshh <sarveshp273@gmail.com>
2021-02-21 16:41:55 +0530

    basic setup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import NavbarItems from './NavbarItems'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>This is navbar</h1>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/Navbar/NavbarItems.js
@@ -0,0 +1,18 @@
+export const NavbarItems = [
+    {
+        title: 'About',
+        link: '/about',
+    },
+    {
+        title: 'Templates',
+        link: '/templates',
+    },
+    {
+        title: 'Learn',
+        link: '/learn',
+    },
+    {
+        title: 'Developer',
+        link: '/abput-me',
+    },
+]
